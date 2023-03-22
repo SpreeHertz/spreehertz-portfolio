c850f4eefc930de584ef41880f317a2b41c5d354
SpreeHertz <spreehertzofficial@gmail.com>
2023-03-22 23:37:13 +0530

    feat: revamp projects (1/?)

---
+++ src/components/GitHubOverlayBtn.vue
@@ -0,0 +1,13 @@
+<template>
+<div class="absolute inset-0 flex justify-end m-3">
+<button class="w-10 h-10 rounded-full flex justify-center items-center cursor-pointer">
+    <img src="../assets/github.svg" alt="github overlay" class="w-1/2 h-1/2 object-contain">
+</button>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'GithubOverlay'
+}
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,14 +1,19 @@
 <template>
   <Navbar />
       <h1 class="font-inter font-semibold text-slate-200 text-3xl pl-9">About</h1>
-    <p class="font-inter font-semibold text-slate-300 text-2xl max-w-2xl pl-9">I am a highly passionate 15-year-old JavaScript/web developer based in India, mainly focusing on web applications made on Vue, Discord and Minecraft bots, Instagram utilities and more. I also have more than 2 years of adminstrating 20+ Discord servers. </p>
-    <h1 class="font-inter font-semibold text-slate-200 text-3xl pl-8 pt-3">Socials</h1>
-    <div class="flex gap-2 pl-6 pt-2">
-      <GithubButton />
-      <InstagramButton />
-      <TwitterButton />
+    <p class="font-inter font-semibold text-slate-300 text-2xl max-w-1xl pl-9">I am a highly passionate 15-year-old JavaScript/web developer based in India, mainly focusing on web applications made on Vue, Discord and Minecraft bots, Instagram utilities and more. I also have more than 2 years of adminstrating 20+ Discord servers. </p>
+    <h1 class="font-inter font-semibold text-slate-200 text-3xl pl-8 pt-7">Tech Stack</h1>
+     <p class="font-inter font-semibold text-slate-300 text-2xl max-w-1xl pl-9">I mainly use these tools for my projects. </p>
+    <div class="flex gap-2 pl-8 pt-2">
+      <a href="https://vuejs.org"><img src="../assets/vuejs.svg" alt="" class="h-10 w-10 sm:grayscale-0 md:grayscale md:hover:grayscale-0"></a>
+      <a href="https://mongodb.com"><img src="../assets/mongodb.svg" alt="" class="h-10 w-10 sm:grayscale-0 md:grayscale md:hover:grayscale-0"></a>
+      <a href="https://git-scm.com"><img src="../assets/git.svg" alt="" class="h-10 w-10 sm:grayscale-0 md:grayscale md:hover:grayscale-0"></a>
+      <a href="https://nodejs.org"><img src="../assets/node-js.svg" alt="" class="h-10 w-10 sm:grayscale-0 md:grayscale md:hover:grayscale-0"></a>
+      <a href="https://tailwindcss.com"><img src="../assets/tailwindcss.svg" alt="" class="h-10 w-10 sm:grayscale-0 md:grayscale md:hover:grayscale-0"></a>
+      <a href="https://flowbite.com"><img src="../assets/flowbite.svg" alt="" class="h-10 w-10 sm:grayscale-0 md:grayscale md:hover:grayscale-0"></a>
+
+
     </div>
-  
 </template>
 
 <script>
@@ -16,8 +21,6 @@ import Navbar from '../components/Navbar.vue';
 import DiscordButton from '../components/DiscordButton.vue';
 import InstagramButton from '../components/InstagramButton.vue';
 import TwitterButton from '../components/TwitterButton.vue';
-import GithubButton from '../components/GithubButton.vue';
-
 export default {
   name: 'About',
   components: {
@@ -25,7 +28,6 @@ export default {
     DiscordButton,
     InstagramButton,
     TwitterButton,
-    GithubButton,
   }
 }
 </script>
